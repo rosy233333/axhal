5393d7d8dfd2c34f8f6cff3d1a1101e334e5a96c
Azure_Stars <zyj578335934@gmail.com>
2023-05-31 19:07:46 +0800

    feat: 恢复arceos当前进度

--- src/platform/qemu_virt_aarch64/gic.rs
+++ src/platform/qemu_virt_aarch64/gic.rs
@@ -4,7 +4,11 @@ use arm_gic::gic_v2::{GicCpuInterface, GicDistributor};
 use memory_addr::PhysAddr;
 use spinlock::SpinNoIrq;
 
+/// The maximum number of IRQs.
 pub const MAX_IRQ_COUNT: usize = 1024;
+
+/// The timer IRQ number.
+pub const TIMER_IRQ_NUM: usize = 30; // physical timer, type=PPI, id=14
 
 const GIC_BASE: usize = 0x0800_0000;
 const GICD_BASE: PhysAddr = PhysAddr::from(GIC_BASE);
@@ -16,22 +20,37 @@ static GICD: SpinNoIrq<GicDistributor> =
 // per-CPU, no lock
 static GICC: GicCpuInterface = GicCpuInterface::new(phys_to_virt(GICC_BASE).as_mut_ptr());
 
+/// Enables or disables the given IRQ.
 pub fn set_enable(irq_num: usize, enabled: bool) {
     GICD.lock().set_enable(irq_num as _, enabled);
 }
 
+/// Registers an IRQ handler for the given IRQ.
+///
+/// It also enables the IRQ if the registration succeeds. It returns `false` if
+/// the registration failed.
 pub fn register_handler(irq_num: usize, handler: IrqHandler) -> bool {
     crate::irq::register_handler_common(irq_num, handler)
 }
 
+/// Dispatches the IRQ.
+///
+/// This function is called by the common interrupt handler. It looks
+/// up in the IRQ handler table and calls the corresponding handler. If
+/// necessary, it also acknowledges the interrupt controller after handling.
 pub fn dispatch_irq(_unused: usize) {
     GICC.handle_irq(|irq_num| crate::irq::dispatch_irq_common(irq_num as _));
 }
 
-pub(super) fn init() {
+/// Initializes GICD, GICC on the primary CPU.
+pub(super) fn init_primary() {
+    info!("Initialize GICv2...");
     GICD.lock().init();
+    GICC.init();
 }
 
-pub(super) fn init_percpu(_cpu_id: usize) {
+/// Initializes GICC on secondary CPUs.
+#[cfg(feature = "smp")]
+pub(super) fn init_secondary() {
     GICC.init();
 }
